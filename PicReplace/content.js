9c9e18a8771fdfdd75fa58c9571ff051411ee866
isabelledelmundo <isabelledelmundo@gmail.com>
2017-01-22 09:21:28 -0500

    chrome ext

--- PicReplace/content.js
+++ PicReplace/content.js
@@ -3,11 +3,7 @@ for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'http://placekitten.com/' + images[i].width + '/' + images[i].height;
 }
 
-var sourceWordsToTargetWords = [
-    [['Donald Trump', 'Trump', 'Donald J. Trump', 'Mr. Trump'], 'Kanye'],
-];
-
-var elements = document.getElementsByTagName('*');
+/var elements = document.getElementsByTagName('*');
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -16,13 +12,12 @@ for (var i = 0; i < elements.length; i++) {
         var node = element.childNodes[j];
 
         if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var replacedText = text.replace(/donald_trump/gi,"kanye2020");
-            //var replacedText1 = text.replace(/Trump/gi, "#kanye2020");
+            var text = node.nodeValue
+            var replacedText = text.replace("Trump", "#kanye2020");
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
-                //element.replaceChild(document.createTextNode(replacedText1), node);
+
             }
         }
     }
